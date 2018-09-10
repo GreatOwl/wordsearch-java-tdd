9bd645ec7a5a7667a305a86176c0260871e345db
Andrew Nowak <andrew@nowakfamily.net>
2018-09-10 08:54:21 -0400

    can get x coordinate

---
+++ src/main/java/com/pillar/wordsearch/Coordinate.java
@@ -0,0 +1,19 @@
+package com.pillar.wordsearch;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Coordinate {
+
+    private int x;
+    private int y;
+
+    public Coordinate (int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+}
---
+++ src/test/java/com/pillar/wordsearch/CoordinateTest.java
@@ -0,0 +1,18 @@
+import com.pillar.wordsearch.Coordinate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+import org.junit.Before;
+import static junit.framework.TestCase.assertEquals;
+
+public class CoordinateTest {
+
+    @Test
+    public void whenCoordinatesConstructedXCanBeRetreived() {
+        int x = 12;
+        int y = 22;
+        Coordinate coord = new Coordinate(x, y);
+        assertEquals(x, coord.getX());
+    }
+}
