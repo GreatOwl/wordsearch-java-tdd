4ae821aca3a5ebb0515110cbdb176563430d50bc
Andrew Nowak <andrew@nowakfamily.net>
2018-09-11 16:06:02 -0400

    refactor mainTest to prep for challenges

--- src/test/java/com/pillar/wordsearch/MainTest.java
+++ src/test/java/com/pillar/wordsearch/MainTest.java
@@ -6,21 +6,28 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class MainTest {
 
-    @Test
-    public void whenFileIsProvidedFileIsReadIntoMemory() {
-        Main main = new Main();
-        String filename = "puzzle1.txt";
+    private Main main;
+    private String testContents;
+    private String filename;
+    @Before
+    public void setup() {
+        filename = "puzzle1.txt";
+        main = new Main();
         String testPath = System.getProperty("user.dir") + "/puzzles/" + filename;
-        String testContents;
         try {
             testContents = new String(Files.readAllBytes(Paths.get(testPath)));
         } catch (IOException e) {
             testContents = "";
         };
+    }
+
+    @Test
+    public void whenFileIsProvidedFileIsReadIntoMemory() {
         assertEquals(testContents, main.getFileContents(filename));        
     }
 }
