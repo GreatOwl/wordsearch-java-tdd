fe76ea45c44b6b654b6c957e7d374bdeb883ddbd
Andrew Nowak <andrew@nowakfamily.net>
2018-09-11 16:41:54 -0400

    Puzzle test lied to me because I made bad assertions

--- src/test/java/com/pillar/wordsearch/MainTest.java
+++ src/test/java/com/pillar/wordsearch/MainTest.java
@@ -81,7 +81,7 @@ public class MainTest {
 
     @Test
     public void whenRawPuzzleGivenPuzzleObjectReturned() {
-        String expectedCompressed = "";
+        String expectedCompressed = "UMKHULKINVJOCWELLSHKZZWZCGJUYGHSUPJPRJDHSBXTGBRJSOEQETIKKGLEAYOAGCIRDQHRTCDSCOTTYKZREPPXPFBLQSLNEEEVULFMZOKRIKAMMRMFBAPPNUIIYHQMEMQRYFSEYZYGKQJPCQWYAKSJFZMQIBDBEMKWDTGLBHCBECHTOYIKOJYEULNCCLYBZUHWZMISUKURBIDUXSKYLBQQPMDFCKEAB";
         String[] testList = {
             "BONES,KHAN,KIRK,SCOTTY,SPOCK,SULU,UHURA",
             "U,M,K,H,U,L,K,I,N,V,J,O,C,W,E",
@@ -101,6 +101,14 @@ public class MainTest {
             "K,Y,L,B,Q,Q,P,M,D,F,C,K,E,A,B"
         };
 
-        assertEquals(testList, main.splitIntoLines(testContents));
+        Puzzle puzzle = main.buildPuzzle(testList);
+        assertEquals(expectedCompressed, puzzle.compressPuzzle().toString());
     }
 }
+// BONES: (0,6),(0,7),(0,8),(0,9),(0,10)
+// KHAN: (5,9),(5,8),(5,7),(5,6)
+// KIRK: (4,7),(3,7),(2,7),(1,7)
+// SCOTTY: (0,5),(1,5),(2,5),(3,5),(4,5),(5,5)
+// SPOCK: (2,1),(3,2),(4,3),(5,4),(6,5)
+// SULU: (3,3),(2,2),(1,1),(0,0)
+// UHURA: (4,0),(3,1),(2,2),(1,3),(0,4)
