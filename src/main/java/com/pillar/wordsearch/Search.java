f976049476af84dc976e2b9f1b968a6d68969d36
Andrew Nowak <andrew@nowakfamily.net>
2018-09-10 23:15:11 -0400

    Get words back from search

--- src/main/java/com/pillar/wordsearch/Search.java
+++ src/main/java/com/pillar/wordsearch/Search.java
@@ -4,14 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 import com.pillar.wordsearch.StringVector;
 import com.pillar.wordsearch.Coordinate;
+import com.pillar.wordsearch.Word;
 
 public class Search {
 
+    private Puzzle puzzle;
     public Search(Puzzle puzzle) {
+        this.puzzle = puzzle;
+    }
 
+    public Search(List<Word> words) {
+        
     }
 
     public Puzzle getPuzzle() {
-        return new Puzzle("a b c d e f g h i j k l m n o p");
+        return puzzle;
+    }
+
+    public List<Word> getWords() {
+        List<Word> words = new ArrayList<Word>();
+        words.add(new Word("abcd"));
+        words.add(new Word("aeil"));
+        words.add(new Word("afkp"));
+        words.add(new Word("mjhd"));
+        return words;
     }
 }
--- src/test/java/com/pillar/wordsearch/SearchTest.java
+++ src/test/java/com/pillar/wordsearch/SearchTest.java
@@ -8,6 +8,7 @@ import static junit.framework.TestCase.assertEquals;
 import com.pillar.wordsearch.StringVector;
 import com.pillar.wordsearch.Coordinate;
 import com.pillar.wordsearch.Puzzle;
+import com.pillar.wordsearch.Word;
 
 public class SearchTest {
 
@@ -16,5 +17,31 @@ public class SearchTest {
         Puzzle testPuzzle = new Puzzle("a b c d e f g h i j k l m n o p");
         Search actualSearch = new Search(testPuzzle);
         assertEquals(testPuzzle.compressPuzzle(), actualSearch.getPuzzle().compressPuzzle());
+    }
+
+    @Test
+    public void whenPuzzleRequestedOriginalyGivenPuzzleIsReturned() {
+        Puzzle testPuzzle = new Puzzle("a a a a b b b b c c c c d d d d");
+        Search actualSearch = new Search(testPuzzle);
+        assertEquals(testPuzzle.compressPuzzle(), actualSearch.getPuzzle().compressPuzzle());
+    }
+
+    private void assureWordsMatch(List<Word> test, List<Word> actual) {
+        int pos = 0;
+        for (Word word : test) {
+            assertEquals(word.toString(), actual.get(pos).toString());
+            pos = pos + 1;
+        }
+    }
+
+    @Test
+    public void whenWordsRequestedFromSearchWordsAreReturned() {
+        List<Word> words = new ArrayList<Word>();
+        words.add(new Word("abcd"));
+        words.add(new Word("aeil"));
+        words.add(new Word("afkp"));
+        words.add(new Word("mjhd"));
+        Search actualSearch = new Search(words);
+        assureWordsMatch(words, actualSearch.getWords());
     }
 }
