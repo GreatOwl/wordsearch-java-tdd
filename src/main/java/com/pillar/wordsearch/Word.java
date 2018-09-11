ffe0fc80d5c2502b6346fec6e5bba1cc25519d85
Andrew Nowak <andrew@nowakfamily.net>
2018-09-10 20:57:34 -0400

    words can reciev coordinates

--- src/main/java/com/pillar/wordsearch/Word.java
+++ src/main/java/com/pillar/wordsearch/Word.java
@@ -8,11 +8,20 @@ import com.pillar.wordsearch.Coordinate;
 public class Word {
 
     private String wordString;
+    private List<Coordinate> coordinates;
     public Word(String wordString) {
         this.wordString = wordString;
     }
 
     public String toString() {
-        return "word";
+        return wordString;
+    }
+
+    public void attach(List<Coordinate> coordinates) {
+        this.coordinates = coordinates;
+    }
+
+    public List<Coordinate> getCoordinates() {
+        return coordinates;
     }
 }
--- src/test/java/com/pillar/wordsearch/WordTest.java
+++ src/test/java/com/pillar/wordsearch/WordTest.java
@@ -17,4 +17,27 @@ public class WordTest {
         assertEquals(wordString, word.toString());
     }
 
+    @Test
+    public void whenAnyWordStringIsGivenItCanBeRetrieved() {
+        String wordString = "balloon";
+        Word word = new Word(wordString);
+        assertEquals(wordString, word.toString());
+    }
+
+    public void whenWordIsFoundCoordinatesCanBeAttachedAndRetreived() {
+        String wordString = "balloon";
+        Word word = new Word(wordString);
+        List<Coordinate> wordCoordinates = new ArrayList<Coordinate>();
+        wordCoordinates.add(new Coordinate(1,2));
+        wordCoordinates.add(new Coordinate(1,3));
+        wordCoordinates.add(new Coordinate(1,4));
+        wordCoordinates.add(new Coordinate(1,5));
+        wordCoordinates.add(new Coordinate(1,6));
+        wordCoordinates.add(new Coordinate(1,7));
+        wordCoordinates.add(new Coordinate(1,8));
+        
+        word.attach(wordCoordinates);
+        assertEquals(wordCoordinates, word.getCoordinates());
+    }
+
 }
